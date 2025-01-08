9b6e284996910097d6222756ccbb0b90a1f933ff
Khatri uma <mohammadumarkhatri650@gmail.com>
2025-01-08 15:13:36 +0530

    Hello Khatri

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./layout/*.liquid",
+    "./sections/*.liquid"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
